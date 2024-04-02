877a3f0a7cc95ccfdf08d146449fe44a3b404e50
WALLACE NUNES DOS SANTOS <wallace.nsantos1@senacsp.edu.br>
2024-04-02 13:48:31 -0300

    aula dia 28-04

---
+++ 2-operadores/js/scripts.js
@@ -0,0 +1,66 @@
+console.log(typeof 2)
+console.log(typeof 2.2)
+console.log(typeof -4)
+console.log("Rodolfo")
+console.log(typeof "Rodolfo")
+
+// 2- operações arítimetica
+
+console.log(2+4)
+console.log(10 -5)
+console.log(5*4)
+console.log(10/2)
+
+// 3 - Special numbers
+
+console.log(typeof Infinity)
+
+console.log(12 * "asd")
+
+// strings
+
+console.log("Um texto")
+console.log('outro texto')
+
+console.log(`Mais um texto`)
+console.log(typeof "Um texto")
+console.log(typeof 'outro texto')
+console.log(typeof `Mais um texto`)
+
+//  5 - Símblos especiais em strings
+
+console.log("Testando \n quebra de linha")
+console.log("Espaçamento \t de tab")
+
+// 6 - Concatenação
+
+console. log("Oi , " + "tudo" + "bem?")
+
+console.log( `Testando uma` + "concatenação")
+
+// 7 - Interpolação 
+
+console(`A soma de 2 + 2 é: ${2+2}`)
+console("A soma de 2 + 2 é: "(2+2))
+
+console.log( `Podemos executar qualquer coisa ${console.log('teste 👵')}`)
+
+// 08 - Booleans
+
+console.log(5 > 2)
+console.log(2 > 5)
+console.log(typeof(2 > 5))
+console.log(10 == 10)
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 == 9)
+console.log(10 != '9')
+console.log(10 !== '10')
+
+// 11 - op logico na prática
+console.log(true && true)
+console.log(true && false)
+
+console.log(5>2 && 2<10)
+
+console.log(10> 2  || 5>6)
---
+++ 3-estruturas/js/script.js
@@ -0,0 +1,74 @@
+let nome ="Rodolfo"
+console.log(nome)
+
+nome = "Rodolfo Gonçalves"
+console.log(nome)
+
+const idade = 38
+console.log(idade)
+
+// idade = 39
+// console.log(idade)
+
+// console.log(`O nome do usuário é: ${nome} essa pessoa tem ${idade} anos`)
+
+
+// constage = prompt("Digite o seu nome")
+// // console.log("O nome digiado foi" + name1)
+// console.log(`O nome digiado foi" + ${name1}`)
+
+console.log("teste")
+console.error("Deu ruim!😥")
+console.warn("Alerta 😯")
+
+// const m = 10
+
+// if(m >=5) {
+//     console.log("M é maior que 5" + "valor de M = " + m)
+// }
+
+// const user = prompt("Digite o seu nome")
+
+// if(user == 'Rodolfo'){
+//     console.log(`Bem vindo ${user}`)
+// }else{
+//     console.log(`Uhm ... novo por aqui ${user}`
+//     )
+// }
+
+
+// const nota = prompt("Digita a média final")
+
+// if(nota >= 7){
+//     console.log("Aprovado")
+// } else if (nota > 5){
+//     console.log("Recuperação")
+// } else{
+//     console.log("Reprovado")
+// }
+
+const diaSemana = parseInt(prompt("Dia da Semana"))
+
+if(diaSemana = 1){
+    alert("Segunda")
+}
+else if(diaSemana = 2){
+    alert("Terça")
+}
+else if(diaSemana = 3){
+    alert("Quarta")
+}
+else if(diaSemana = 4){
+    alert("Quinta")
+}
+else if(diaSemana = 5){
+    alert("Sexta")
+}
+else if(diaSemana = 6){
+    alert("Sabado")
+}
+else if(diaSemana = 7){
+    alert("Domingo")
+}else{
+    alert("Dia Inválido")
+}
